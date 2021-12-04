6db1ac7234a956df530fc9cb821a42392a979916
tungnd237 <tungtorres23@gmail.com>
2021-12-04 16:16:01 +0700

    message

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = ['django-env.eba-eqwzeutp.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
