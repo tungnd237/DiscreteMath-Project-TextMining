5e90f952b90a09083bfcb84ab954c6b2e7c65c8c
tungnd237 <tungtorres23@gmail.com>
2021-12-04 18:51:58 +0700

    change allow host

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.appseed-srv1.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.appseed-srv1.com', 'my-env.eba-ja9fxpta.us-east-1.elasticbeanstalk.com']
 
 # Application definition
 
